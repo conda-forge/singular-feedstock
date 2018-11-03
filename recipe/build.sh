a17c9fb96e690d6cc505f8311e3f8ba3740a4ddd
Isuru Fernando <isuruf@gmail.com>
2018-11-03 00:45:56 -0500

    Remove warning in oMalloc

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ export CPPFLAGS="-I$PREFIX/include -DDISABLE_COMMENTATOR $CPPFLAGS"
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export CFLAGS="-O2 -g -fPIC $CFLAGS"
-export CXXFLAGS="-O2 -g -fPIC $CXXFLAGS"
+export CXXFLAGS="-O2 -g -fPIC $CXXFLAGS -Wno-deprecated-register"
 
 chmod +x configure
 
